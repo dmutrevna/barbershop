b760da02c505a5a8c7594c003e398ed19ec25d57
dmutrevna <101204568+dmutrevna@users.noreply.github.com>
2023-07-15 14:37:06 +0200

    no-scroll

--- js/modal.js
+++ js/modal.js
@@ -3,14 +3,17 @@
   const closeModalBtns = document.querySelectorAll('[data-modal-close]');
   const modal = document.querySelector('[data-modal]');
   const modalOverlay = document.querySelector('.modal-overlay');
+  const body = document.querySelector('body');
 
   function toggleModal() {
     modal.classList.toggle('is-hidden');
 
     if (!modal.classList.contains('is-hidden')) {
+      body.classList.add('modal-open');
       document.addEventListener('keydown', closeModalOnEsc);
       modalOverlay.addEventListener('click', closeModalOnBackdrop);
     } else {
+      body.classList.remove('modal-open');
       document.removeEventListener('keydown', closeModalOnEsc);
       modalOverlay.removeEventListener('click', closeModalOnBackdrop);
     }
