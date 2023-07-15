1d9fdcc031e4f2909f6d6e840e38edb6870547f8
dmutrevna <101204568+dmutrevna@users.noreply.github.com>
2023-07-15 14:29:24 +0200

    modal

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,33 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelectorAll('[data-modal-open]'),
-    closeModalBtn: document.querySelectorAll('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.forEach(function (btn) {
-    btn.addEventListener('click', toggleModal);
-  });
-
-  refs.closeModalBtn.forEach(function (btn) {
-    btn.addEventListener('click', toggleModal);
-  });
+  const openModalBtns = document.querySelectorAll('[data-modal-open]');
+  const closeModalBtns = document.querySelectorAll('[data-modal-close]');
+  const modal = document.querySelector('[data-modal]');
+  const modalOverlay = document.querySelector('.modal-overlay');
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    modal.classList.toggle('is-hidden');
+
+    if (!modal.classList.contains('is-hidden')) {
+      document.addEventListener('keydown', closeModalOnEsc);
+      modalOverlay.addEventListener('click', closeModalOnBackdrop);
+    } else {
+      document.removeEventListener('keydown', closeModalOnEsc);
+      modalOverlay.removeEventListener('click', closeModalOnBackdrop);
+    }
   }
+
+  function closeModalOnEsc(e) {
+    if (e.code === 'Escape') {
+      toggleModal();
+    }
+  }
+
+  function closeModalOnBackdrop(e) {
+    if (e.target === modalOverlay) {
+      toggleModal();
+    }
+  }
+
+  openModalBtns.forEach(btn => btn.addEventListener('click', toggleModal));
+  closeModalBtns.forEach(btn => btn.addEventListener('click', toggleModal));
 })();
